48c067b5492c9605046caaae4e90aec25d8fd2b2
Avery-AN <avery_mail@163.com>
2020-03-22 11:10:39 +0800

    bug fixes

--- TableView/AdvancedTableView/AdvancedCell+SelfManager.h
+++ TableView/AdvancedTableView/AdvancedCell+SelfManager.h
@@ -10,14 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-//typedef void (^GetStytleCompletionBlock)(NSInteger start, NSInteger end);
-
 @interface AdvancedCell (SelfManager) <QAAttributedLabelProperty>
-
-//@property (nonatomic, copy) GetStytleCompletionBlock completionBlock;
-
-//- (void)getStytle:(NSMutableArray *)datas maxConcurrentOperationCount:(NSInteger)maxConcurrentOperationCount
-//       completion:(GetStytleCompletionBlock)completion;
 
 - (void)getStyleWithQueue:(dispatch_queue_t)dispatchQueue
             dispatchGroup:(dispatch_group_t)dispatchGroup
--- TableView/AdvancedTableView/TrapezoidalCell+SelfManager.h
+++ TableView/AdvancedTableView/TrapezoidalCell+SelfManager.h
@@ -8,16 +8,10 @@
 
 #import "TrapezoidalCell.h"
 
-//typedef void (^GetStytleCompletionBlock)(NSInteger start, NSInteger end);
-
 NS_ASSUME_NONNULL_BEGIN
 
 @interface TrapezoidalCell (SelfManager) <QAAttributedLabelProperty>
 
-//@property (nonatomic, copy) GetStytleCompletionBlock completionBlock;
-
-//- (void)getStytle:(NSMutableArray *)datas maxConcurrentOperationCount:(NSInteger)maxConcurrentOperationCount
-//       completion:(GetStytleCompletionBlock)completion;
 - (void)getStyleWithQueue:(dispatch_queue_t)dispatchQueue
             dispatchGroup:(dispatch_group_t)dispatchGroup
                       dic:(NSMutableDictionary *)dic
